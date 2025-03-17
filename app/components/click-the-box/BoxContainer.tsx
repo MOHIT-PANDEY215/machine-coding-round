97816a9d070d5b705819350875fdb5a04aa96faf
Mohit Pandey <pandeymohit215@gmail.com>
2025-03-17 22:42:00 +0530

    fix: click the box bug(disable click on setTimeout)

--- app/components/click-the-box/BoxContainer.tsx
+++ app/components/click-the-box/BoxContainer.tsx
@@ -58,12 +58,14 @@ interface BoxDataProps {
 const BoxContainer: React.FC = () => {
   const [data, setData] = useState<BoxDataProps[]>([]);
   const [indexQueue, setIndexQueue] = useState<number[]>([]);
+  const [isClickAllowed,setIsClickAllowed] = useState<boolean>(true)
 
   useEffect(() => {
     setData(boxData);
   }, []);
   useEffect(() => {
     if (indexQueue.length === 7) {
+      setIsClickAllowed(false)
       for (let i = 0; i < indexQueue.length; ++i) {
         setTimeout(() => {
           setData((prev) => {
@@ -82,30 +84,36 @@ const BoxContainer: React.FC = () => {
     }
   }, [indexQueue]);
 
-  const handleBoxClick = (item: BoxDataProps) => {
-    if (item.isClicked) return;
+  const handleBoxClick = (e:React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    console.log(indexQueue,boxData)
+    const isClicked = (e.target as HTMLInputElement).getAttribute('data-isclicked')
+    const id =  Number((e.target as HTMLInputElement).getAttribute('data-id'))
+    if(isClicked === 'hidden' || isClicked === null) return;
+    if(indexQueue.length === 0) setIsClickAllowed(true)
+    if (isClicked==='true' || (!isClickAllowed && indexQueue.length !== 0)) return;
     setData((prev) => {
       const temp = prev?.map((ele) => {
-        if (item.id !== ele.id) return ele;
+        if (id !== ele.id) return ele;
         return { ...ele, isClicked: true };
       });
       return temp;
     });
     setIndexQueue((prev) => {
       const temp = [...prev];
-      temp.push(item.id);
+      temp.push(id);
       return temp;
     });
   };
 
   return (
-    <div className="grid grid-cols-3  gap-4">
+    <div className="grid grid-cols-3  gap-4" onClick={(e)=>{handleBoxClick(e)}}>
       {data?.map((item, index) => {
-        if (!item.isVisible) return <div key={index}></div>;
+        if (!item.isVisible) return <div key={index} data-isclicked={`hidden`}></div>;
         return (
           <div
             key={index}
-            onClick={() => handleBoxClick(item)}
+            data-id={item.id}
+            data-isclicked={item.isClicked}
             className={`w-[80px] h-[80px] ${
               item.isClicked ? "bg-green-700" : "bg-yellow-300"
             } text-black text-lg flex items-center justify-center cursor-pointer`}

e7d4d244301f535c7ef9692450766a1ad6e49276
Mohit Pandey <pandeymohit215@gmail.com>
2025-03-28 23:41:31 +0530

    add: overlapping circles

---
+++ app/(projects)/detect-overlapping-circles/page.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/app/components/layout/Layout";
+
+export default function page() {
+  return (
+    <>
+      <Layout title="Detect Overlapping Circles">
+        Circle
+      </Layout>
+    </>
+  );
+}
